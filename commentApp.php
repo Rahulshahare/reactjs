0445f5e95c9cef8703d1ac35a6f0909ba9688e89
Rahul Shahare <rahulshahare@gmail.com>
2017-09-10 03:08:10 +0530

    JustBegin

---
+++ commentApp.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ATM Machine</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <script src="js/react.min.js"></script>
+        <script src="js/react-dom.min.js"></script>
+        <script src="js/react-with-addons.js"></script>
+        <script src="js/browser.min.js"></script>
+
+        <style type="text/css">
+        
+        </style>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div id="App"></div>
+                </div>
+            </div>
+        </div>
+                                    
+        <script type="text/babel">
+        var Comment = React.createClass({
+            render:function(){
+                return(
+                    "Hello World";
+                )
+            }
+        })
+        </script>
+    </body>
+    </html>
