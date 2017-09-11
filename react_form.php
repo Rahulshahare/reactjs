fedf04add8fdf5e727edd7d26173b79db85e0c61
Rahul Shahare <rahulshahare@gmail.com>
2017-09-11 12:58:14 +0530

    Working on Forms and its data in React

---
+++ react_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>React Form</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <script src="js/react.min.js"></script>
+        <script src="js/react-dom.min.js"></script>
+        <script src="js/react-with-addons.js"></script>
+        <script src="js/browser.min.js"></script>
+
+        <style type="text/css">
+        </style>
+
+    </head>
+    <body>
+    </body>
+</html>
