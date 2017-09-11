3b98043950242d4e2f021b1d69e89950a133e5ab
Rahul Shahare <rahulshahare@gmail.com>
2017-09-11 12:56:31 +0530

    title change

--- commentApp.php
+++ commentApp.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ATM Machine</title>
+        <title>COmment System</title>
         <link href="css/bootstrap.css" rel="stylesheet">
         <script src="js/react.min.js"></script>
         <script src="js/react-dom.min.js"></script>
