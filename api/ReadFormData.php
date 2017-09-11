7c8b1bbb5f02e2ae816b4cd176be838edbe42574
Rahul Shahare <rahulshahare@gmail.com>
2017-09-11 19:50:52 +0530

    its working state

--- api/ReadFormData.php
+++ api/ReadFormData.php
@@ -1,8 +1,4 @@
 <?php
-
-
-
-
 if(count($_POST)>0){
     $email = $_POST['Email'];
     $name = $_POST['Name'];
@@ -14,7 +10,21 @@ if(count($_POST)>0){
               DOB is {$dob},<br/>
               and Mobile Number is {$mobile}.";
               echo $data;
+    $value = [
+    "Name" => "{$name}",
+    "Email" => "{$email}",
+    "dob"=> "{$dob}",
+    "Mobile"=>"{$mobile}"
+];
+    
+    echo"New Array is";
+    echo "<Script type=\"text/javascript\">";
+    echo "var NewData = ".json_encode($value).";";
+    echo"</script>";
+    print_r($value);
+
 }else{
     echo"No data receive";
 }
+
 ?>
