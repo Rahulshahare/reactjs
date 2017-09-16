36792cfb64136964ff1d15b5d1ad275aabb78976
Rahul Shahare <rahulshahare@gmail.com>
2017-09-16 16:32:49 +0530

    Page Set Up

---
+++ cms.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>React Learning</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <script src="js/react.min.js"></script>
+    <script src="js/react-dom.min.js"></script>
+    <script src="js/browser.min.js"></script>
+  </head>
+    <body>
+    </body>
+    <<script type="text/babel"></script>
+</html>
