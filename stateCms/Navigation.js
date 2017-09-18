5ebe0589f833447b8a57c379eca79b409af89861
Rahul Shahare <rahulshahare@gmail.com>
2017-09-19 02:26:40 +0530

    Working well

---
+++ stateCms/Navigation.js
@@ -0,0 +1,11 @@
+var Nevigation = React.createClass({
+    render:function(){
+        return(
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">    
+                    <a className="navbar-brand newFont" href="#">Instagram</a>
+                </div>
+            </nav>
+        );
+    }
+});
--- stateCms/ReadState.js
+++ stateCms/ReadState.js
@@ -1,27 +1,32 @@
-    var ReadState = React.createClass({
-            getInitialState:function(){
-                return({
-                    States:[],
-                })
-            },
-            componentDidMount(){
-                $.ajax({
-                     url: "http://localhost/oceangreen/admin/api/readState.php",
-                     type : "GET",
-                     cache: false,
-                     success: function(html)
-                         {
-                             this.setState({States:JSON.parse(html)});
-                             alert("first"+this.state.States);
-                         }.bind(this),
-                 });
-     },
+var ReadState = React.createClass({
+    getInitialState:function(){
+        return({
+            States:[],
+        })
+    },
+    componentDidMount(){
+        $.ajax({
+             url: "http://localhost/oceangreen/admin/api/readState.php",
+             type : "GET",
+             cache: false,
+             success: function(html)
+                 {
+                     this.setState({States:JSON.parse(html)});
+                     alert("first"+this.state.States);
+                 }.bind(this),
+         });
+    },
 
-     render:function(){
+    render:function(){
         return(
-            <h1>Hello</h1>
-        )
-     },
+            <div className="panel panel-primary">
+                <div className="panel-heading">SHowing States</div>
+                <div className="panel-body">
+                    <ShowAllState AllState={this.state.States} ChangeAppMode={this.props.ChangeAppMode}/>
+                </div>
+            </div>
+        );
+    },
 
 
-    });
+});
