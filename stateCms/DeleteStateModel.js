7b08679bd2b59b005800d6aab98cd663c3616dbd
Rahul Shahare <rahulshahare@gmail.com>
2017-10-10 03:41:31 +0530

    Modal Ready to work

---
+++ stateCms/DeleteStateModel.js
@@ -0,0 +1,128 @@
+window.DeleteStateModel = React.createClass({
+    getInitialState:function(){
+        return({
+            StateName: this.props.StateName,
+            StateId:this.props.StateId,
+            Error:'',
+            BtnName:'Delete',
+            BtnState:'',
+        })
+    },
+    Close:function(){
+        this.setState({
+            StateName:'',
+            StateId:'',
+            Error:'',
+        })
+
+        this.props.Hidemodal()
+    },
+    DeleteStateNow:function(){
+        var dataString = 'StateId='+ this.props.StateId;
+        //alert(dataString);
+        this.setState({
+            BtnName:'Deleting',
+            BtnState:'disabled',
+        })
+        
+            $.ajax({
+                url: "http://localhost/oceangreen/admin/api/DeleteState.php",
+                type : "POST",
+                data:dataString,
+                cache: false,
+                success: function(html)
+                {
+                    //alert(html);
+                    this.setState({
+                                    Error:html,
+                                    BtnName:'Delete',
+                                    BtnState:'',
+                    })
+                    if(html=='Deleted'){
+                        this.props.Refresh();
+                    }
+                                        
+                }.bind(this),
+            });
+    },
+    LandingOption:function(){
+        return(
+            <div>
+                <div className="modal-body">
+                <p><strong>Are you sure want to delete {this.props.StateName} State?</strong></p>
+                </div>
+                <div className="modal-footer">
+                <button type="button" onClick={this.DeleteStateNow} className="btn btn-danger btn-xs" disabled={this.state.BtnState}>{this.state.BtnName}</button>
+                <button type="button" onClick={this.Close} className="btn btn-default btn-xs">Cancel</button>
+                </div>
+            </div>
+        )
+    },
+    OnDelete:function(){
+        return(
+                <div>
+                    <div className="modal-body">
+                    <p><strong>{this.props.StateName} is Deleted Successfully.</strong></p>
+                    </div>
+                    <div className="modal-footer">
+                    <button type="button" onClick={this.Close} className="btn btn-primary btn-xs">Done</button>
+                    </div>
+                </div>
+            )
+    },
+    CanNotDelete:function(){
+        return(
+            <div>
+                <div className="modal-body">
+                <p><strong>You can't Delete {this.props.StateName} State as it haves Districts in it.</strong></p>
+                </div>
+                <div className="modal-footer">
+                <button type="button" onClick={this.Close} className="btn btn-primary btn-xs">Ok</button>
+                </div>
+            </div>
+        )
+    },
+    AnyError:function(){
+        return(
+            <div>
+                <div className="modal-body">
+                <p><strong>Internal Server Error Occured.</strong></p>
+                </div>
+                <div className="modal-footer">
+                <button type="button" onClick={this.Close} className="btn btn-primary btn-xs">Ok</button>
+                </div>
+            </div>
+        )
+    },
+    render:function(){
+        if(!this.props.ShowModel){
+            return null;
+        }
+        var Operations;
+        switch(this.state.Error){
+            case '': Operations = this.LandingOption();
+            break;
+            case 'Deleted' : Operations = this.OnDelete();
+            break;
+            case 'CanNotDelete' : Operations = this.CanNotDelete();
+            break;
+            case 'Contact Administrator' :Operations = this.AnyError();
+            break;
+        }
+
+
+        return(
+            <div className="modal draggable show"  id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
+                <div className="modal-dialog modal-sm">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                                    <button type="button" onClick={this.Close} className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <span className="modal-title" id="myModalLabel">Delete State</span>
+                        </div>
+                        {Operations}
+                    </div>
+                </div>
+            </div>
+            )
+    }
+});
