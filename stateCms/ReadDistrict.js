13bda41ae8b11af8d94adf093fbbd1f9223950d4
Rahul Shahare <rahulshahare@gmail.com>
2017-11-13 22:20:51 +0530

    its working for now

--- stateCms/ReadDistrict.js
+++ stateCms/ReadDistrict.js
@@ -21,30 +21,62 @@ window.ReadDistrict = React.createClass({
         });
     },
     toggleAddModel:function(){
+        var arr = this.state.Districts;
+        var L =arr[1]['id'];
+        var NewArr;
+        var obj = {
+            length: 0,
+        
+            addElem: function addElem(elem) {
+                // obj.length is automatically incremented 
+                // every time an element is added.
+                [].push.call(this, elem);
+            }
+        };
+        
+       
+        console.log(obj.length);
+
+        for (var index = 0; index < 4; index++) {
+            obj.addElem(arr[index]);
+            
+            
+        }
+
+        alert(L);
+        console.log(obj);
         this.setState({
             isOpenAdd:!this.state.isOpenAdd
         })
     },
     render:function(){
         return(
-            <div className="panel panel-primary">
-                <div className="panel-heading">
-                    Showing States
-                    <CommomButton 
-                        BtnType={"primary"} 
-                        onClick={this.toggleAddModel}  
-                        BtnName={"Add New District"}
-                        Align={"right"}
-                    />
+            <div>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">
+                        Showing States
+                        <CommomButton 
+                            BtnType={"primary"} 
+                            onClick={this.toggleAddModel}  
+                            BtnName={"Add New District"}
+                            Align={"right"}
+                        />
+                        </div>
+                    <div className="panel-body">
+                        <ShowAllDistrict 
+                            AllDistrict={this.state.Districts}  
+                            ChangeAppMode={this.props.ChangeAppMode}
+                            Deleting={this.Deleting}
+                            Editing={this.Editing}
+                        />
                     </div>
-                <div className="panel-body">
-                    <ShowAllDistrict 
-                        AllDistrict={this.state.Districts}  
-                        ChangeAppMode={this.props.ChangeAppMode}
-                        Deleting={this.Deleting}
-                        Editing={this.Editing}
-                    />
                 </div>
+                <ModalAddDistrict
+                ShowModal = {this.state.isOpenAdd}
+                Close={this.toggleAddModel}
+                Refresh = {this.GetDistrictData}
+                DistrictArray={this.state.Districts}
+                />
             </div>
         )
     }
